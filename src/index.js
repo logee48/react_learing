84c12f58fee7ee653de11e56cbb37b83d8e0b3d3
logesh <logeshe9159@gmail.com>
2023-01-10 13:04:50 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// const newroot = ReactDOM.createRoot(document.getElementById('newroot'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const a = <h1>new heading</h1>
+// newroot.render(a)
+
+// function timeing()
+// {
+//   const timeinggg = (
+//     <div>
+//       <div>time now is:</div>
+//       <div>{new Date().toLocaleTimeString()}</div>
+//     </div>
+//   )
+//   newroot.render(timeinggg)
+// }
+// setInterval(timeing,1000)
